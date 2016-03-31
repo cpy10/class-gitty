b17b2d4f60de24eee058fbb77f0ceb761346cec1
cpy10 <joy7g6@gmail.com>
2016-03-31 07:06:50 +0530

    Revert "Wrong age and height values deleted"
    This reverts commit 3cad19a06739cacc950b9c9d1d98d9fc728cbe1d.

--- ex1.c
+++ ex1.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int age = 21;
-    int height = 76;
+    int age = 10;
+    int height = 72;
 
     printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
