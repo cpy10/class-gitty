c6bf06b2b5a4ae66127f5278ef56bd4dd4a093cd
cpy10 <joy7g6@gmail.com>
2016-03-31 07:18:18 +0530

    New Print mesage

--- ex1.c
+++ ex1.c
@@ -5,8 +5,8 @@ int main()
     int age = 21;
     int height = 78;
 
-    printf("I am %d years old.\n", age);
-    printf("I am %d inches tall.\n", height);
+    printf(" You are %d years old.\n", age);
+    printf("You are %d inches tall.\n", height);
 
     return 0;
 }
